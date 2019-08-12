63dc4af5ee2591b69c6211d7b273870917a5ab85
Guilherme Oliveira <guilherme.ol.an@gmail.com>
2019-08-12 19:23:44 -0300

    Adcionando Services e repository

---
+++ src/main/java/com/BancoCapgemini/BancoCapgemini/services/ContaService.java
@@ -0,0 +1,35 @@
+package com.BancoCapgemini.BancoCapgemini.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.BancoCapgemini.BancoCapgemini.models.Conta;
+import com.BancoCapgemini.BancoCapgemini.repositories.ContaRepository;
+@Service
+public class ContaService {
+
+	@Autowired
+	private ContaRepository contaRepository;
+	
+	public Optional<Conta> findByID(Long id){
+		return contaRepository.findById(id);
+	}
+	
+	public List <Conta> findAll(){
+		return contaRepository.findAll();
+	}
+	
+	public Conta save(Conta conta) {
+		return contaRepository.save(conta);
+	}
+	
+	public void deleteByID(long id) {
+		contaRepository.deleteById(id);
+	}
+	public void deleteAll() {
+		contaRepository.deleteAll();
+	}
+}
