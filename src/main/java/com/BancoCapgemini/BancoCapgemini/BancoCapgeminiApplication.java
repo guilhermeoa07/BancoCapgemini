054e2137c5abac8bf63684c6acac608a004f36fc
Guilherme Oliveira <guilherme.ol.an@gmail.com>
2019-08-12 18:41:27 -0300

    Iniciando Projeto

---
+++ src/main/java/com/BancoCapgemini/BancoCapgemini/BancoCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package com.BancoCapgemini.BancoCapgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoCapgeminiApplication.class, args);
+	}
+
+}
